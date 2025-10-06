105300428e8c90d3249b2783908ac4973a801d8b
Swapnali <khairnarswapnalij@gmail.com>
2025-10-06 10:52:37 +0530

    Ex_03-Literals

---
+++ src/Ex_03_Literals/Lab032_Literal_Final.java
@@ -0,0 +1,17 @@
+package Ex_03_Literals;
+
+public class Lab032_Literal_Final {
+    public static void main(String[] args) {
+        int age = 65;
+        // Literal - Integral
+
+        final float pi = 3.14f;
+        //  Literal - float
+
+        final int girl_age = 18;
+        //girl_age = 19;
+
+        System.out.println(girl_age);
+        System.out.println(pi);
+    }
+}
