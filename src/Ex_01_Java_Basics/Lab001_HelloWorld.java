6ab1b8eed46844f912b7727cae59be8ba7041d87
Swapnali <khairnarswapnalij@gmail.com>
2025-10-06 08:23:30 +0530

    Ex_01-Java Basics 13 Sept 25

---
+++ src/Ex_01_Java_Basics/Lab001_HelloWorld.java
@@ -0,0 +1,8 @@
+package Ex_01_Java_Basics;
+
+public class Lab001_HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("This is my first Java Program.");
+        System.out.println("All the very best for your furure.");
+    }
+}
