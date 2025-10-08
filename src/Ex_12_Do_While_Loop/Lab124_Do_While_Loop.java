c001b28fc165ca2c91b0009d946240ebb16331b8
Swapnali <khairnarswapnalij@gmail.com>
2025-10-08 10:36:18 +0530

    Ex_12_Do_While_Loop

---
+++ src/Ex_12_Do_While_Loop/Lab124_Do_While_Loop.java
@@ -0,0 +1,12 @@
+package Ex_12_Do_While_Loop;
+
+public class Lab124_Do_While_Loop {
+    public static void main(String[] args) {
+        int a = 1;
+        do{
+            System.out.println("Hello World!");
+            System.out.println(a);
+            a++;
+        }while (a<10);
+    }
+}
