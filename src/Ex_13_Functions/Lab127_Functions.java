a12731e461c0c8a28d4bc647d5af81e92598017b
Swapnali <khairnarswapnalij@gmail.com>
2025-10-08 11:19:48 +0530

    Ex_13_Functions

---
+++ src/Ex_13_Functions/Lab127_Functions.java
@@ -0,0 +1,10 @@
+package Ex_13_Functions;
+
+public class Lab127_Functions {
+    public static void main(String[] args) {
+        int max = Math.max(3, 4);
+        // Built - in
+
+        System.out.println(max);
+    }
+}
---
+++ src/Ex_13_Functions/Lab128_Method_Functions.java
@@ -0,0 +1,18 @@
+package Ex_13_Functions;
+
+public class Lab128_Method_Functions {
+    public static void main(String[] args) {
+        // Step 2 - Functions
+        // Calling
+        name_of_the_function();
+
+
+    }
+    // Step 1 - Functions
+    // declare, definition
+
+    static void name_of_the_function(){
+        // this is block of code to be executed
+        System.out.println("Hi, there.");
+    }
+}
